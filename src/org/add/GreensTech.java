e459efb94f88ca142c4db8f278102e967a0f14e3
vijivini596 <jvijaya596@gmail.com>
2022-07-09 11:52:47 +0530

    Greens Address

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,25 @@
+package org.add;
+
+
+public class GreensTech {
+	public void greensOmr() {
+		System.out.println("In OMR");
+	}
+public void greensAdayar() {
+	System.out.println("In ADAYAR");
+}
+public void greensTambaram() {
+	System.out.println("In TAMBARAM");
+}
+public void greensVelacherry() {
+	System.out.println("In VELACHERRY");
+}
+public void greensAnnaNagar() {
+	System.out.println("In ANNANAGAR");
+}
+	
+				
+
+	}
+
+
